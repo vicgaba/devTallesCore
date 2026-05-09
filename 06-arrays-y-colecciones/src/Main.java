9faf09befb1aaa5419de6ae02c055bb335f1dd7c
Victor Gabardini <victorgabardini@gmail.com>
2026-05-09 20:52:25 -0300

    Added Arrays, primitive and wrapper

--- 06-arrays-y-colecciones/src/Main.java
+++ 06-arrays-y-colecciones/src/Main.java
@@ -1,32 +1,42 @@
 import org.w3c.dom.ls.LSOutput;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] numbersPrimitive = new int[5];
-        Integer[] numbersWrapper =new Integer[5];
+//        int[] numbersPrimitive = new int[5]; //Estructura estática. Acá le paso datos primitivos pero el new hace referencia a reservar el espacio en memoria
+//        Integer[] numbersWrapper =new Integer[5]; //Estructura dinámica. No puedo usar datos primitivos
+//
+//        numbersPrimitive[0] = 10;
+//        numbersPrimitive[1] = 20;
+//        //Si quiero agregar una posición más o varias
+//        int[] newNumbersPrimitive = new int[numbersPrimitive.length + 1];
+//        System.arraycopy(numbersPrimitive, 0, newNumbersPrimitive, 0, numbersPrimitive.length); // origen, desde que posición, destino, desde que posición, cuantos elementos
+//        newNumbersPrimitive[5] =  500;
+//
+//        numbersWrapper[0] = 10;
+//        numbersWrapper[1] = 20;
+//
+//        System.out.println("Tipo int: ");
+//        for(int number: numbersPrimitive) {
+//            System.out.println(number);
+//        }
+//
+//        System.out.println("Tipo Integer: ");
+//        for(Integer number: numbersWrapper) {
+//            System.out.println(number);
+//        }
+//
+//        System.out.println("Copia nueva de primitivos: ");
+//        for(int number: newNumbersPrimitive){
+//            System.out.println(number);
+//        }
 
-        numbersPrimitive[0] = 10;
-        numbersPrimitive[1] = 20;
-        //Si quiero agregar una posición más o varias
-        int[] newNumbersPrimitive = new int[numbersPrimitive.length + 1];
-        System.arraycopy(numbersPrimitive, 0, newNumbersPrimitive, 0, numbersPrimitive.length);
-        newNumbersPrimitive[5] =  500;
+        int[] numbersPrimitive = {1,2,3,4,5,6,7,8,9};
 
-        numbersWrapper[0] = 10;
-        numbersWrapper[1] = 20;
+        int deletePosition = 2;
 
-        for(int number: numbersPrimitive) {
-            System.out.println(number);
-        }
-
-        for(Integer number: numbersWrapper) {
-            System.out.println(number);
-        }
-
-        for(int number: newNumbersPrimitive){
-            System.out.println(number);
-        }
-
+        
     }
 }
